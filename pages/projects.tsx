7f04a865032b220018f853c70cc03af15c9dbf78
execaus <execaus@gmail.com>
2023-09-24 21:00:38 +0500

    Добавляет переход на паспорт проекта

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,10 @@
 import { Box, Button, Card, Grid, Heading, HStack, Image, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { Page } from '../components/page';
+import { useRouter } from 'next/navigation';
 
 function Content() {
+	const router = useRouter()
 	const [projects, setProject] = useState([
 		{
 			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
@@ -54,7 +56,11 @@ function Content() {
 			</HStack>
 			<Grid gap={8} templateColumns={"repeat(6, 1fr)"}>
 				{
-					projects.map(project => <Card p={4}>
+					projects.map(project => <Card style={{
+						cursor: "pointer",
+					}} p={4} onClick={() => {
+						router.push("/passport")
+					}}>
 						<Image src={project.image} w={300} />
 						<Text paddingTop={3} fontSize='xl'>{ project.name }</Text>
 					</Card>)
