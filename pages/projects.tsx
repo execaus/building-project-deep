d6e5ed6246dc45254099108bc762e632c08eb3cd
execaus <execaus@gmail.com>
2023-09-24 20:54:43 +0500

    Добавляет страницу с проектами

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import { Box, Button, Card, Grid, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { Page } from '../components/page';
-import { useRouter } from 'next/navigation';
-import { VStack } from '@chakra-ui/react';
 
 function Content() {
-	return (
-		<VStack spacing={8}>
+	const [projects, setProject] = useState([
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+		{
+			image: "https://proprikol.ru/wp-content/uploads/2020/09/kartinki-mnogoetazhnyh-domov-20.jpg",
+			name: "Название проекта",
+		},
+	])
 
-		</VStack>
+	return (
+		<Box p={4} w={"100%"}>
+			<HStack paddingBottom={4}>
+				<Heading>Проекты</Heading>
+				<Button marginLeft={"auto"} colorScheme={"green"}>Создать проект</Button>
+			</HStack>
+			<Grid gap={8} templateColumns={"repeat(6, 1fr)"}>
+				{
+					projects.map(project => <Card p={4}>
+						<Image src={project.image} w={300} />
+						<Text paddingTop={3} fontSize='xl'>{ project.name }</Text>
+					</Card>)
+				}
+			</Grid>
+		</Box>
 	);
 }
 
