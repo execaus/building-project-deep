7c48f8ae686fa3b7d1c229827aa25103d9cd7952
oceanzove <zove888@gmail.com>
2023-09-24 15:49:48 +0500

    Добавляет экран подрядчика

--- pages/contractor.tsx
+++ pages/contractor.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import { Page } from '../components/page';
+import {Heading, Card, CardHeader, HStack, VStack} from "@chakra-ui/react";
+import {Box} from "@chakra-ui/layout";
+
 
 function Content() {
 	return (
-		<div>contractor</div>
+		<Box p={20} >
+			<Heading as='h1' size='2xl' textAlign="left">
+				Экран подрядчика
+			</Heading>
+			<VStack spacing={10} align="left" mt={10}>
+				<HStack  spacing={10} >
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+				</HStack>
+				<HStack  spacing={10}>
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+					<Card>
+						<CardHeader>
+							<Heading size='md'>Процесс, в котором он задействован</Heading>
+						</CardHeader>
+					</Card>
+				</HStack>
+			</VStack>
+		</Box>
+
 	);
 }
 
