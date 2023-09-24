4e34c2402ef81df7c546cd2f358673e5c454c6a0
execaus <execaus@gmail.com>
2023-09-24 14:40:13 +0500

    Добавляет CustomBlock

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ function Content() {
 	const navigator = useRouter()
 
 	return (
-		<VStack>
+		<VStack spacing={8}>
 			<Button colorScheme="blue" onClick={() => navigator.push("/projects")}>Проекты</Button>
 			<Button colorScheme="blue">Гость</Button>
 		</VStack>
--- pages/status-list.tsx
+++ pages/status-list.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Page } from '../components/page';
+import { Heading, HStack } from '@chakra-ui/react';
+import StatusList from '../components/statuses/StatusList';
+import StatusSequenceList from '../components/statuses/StatusSequenceList';
 
 function Content() {
 	return (
-		<div>passport</div>
+		<HStack spacing={8}>
+			<Heading>Статусы</Heading>
+			<StatusList />
+			<StatusSequenceList />
+		</HStack>
 	);
 }
 
