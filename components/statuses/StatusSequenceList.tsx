a4691c2d31dc46ab011a386bb71a5bab5fe1be1f
execaus <execaus@gmail.com>
2023-09-24 19:23:09 +0500

    Добавляет создание последовательности статусов

--- components/statuses/StatusSequenceList.tsx
+++ components/statuses/StatusSequenceList.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Box, Button, Card, Flex, HStack, Heading, Stack, VStack, useDisclosure } from '@chakra-ui/react';
 import StatusSequencePreview, { StatusSequencePreviewProps } from './StatusSequencePreview';
 import { Add } from '@mui/icons-material';
+import { useRouter } from 'next/navigation';
 
 const StatusSequenceList = () => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-
+	const router = useRouter()
 	const list: StatusSequencePreviewProps[] = [
 		{
 			name: 'Последовательность статусов',
@@ -83,17 +83,21 @@ const StatusSequenceList = () => {
 		},
 	]
 
+	const onCreate = () => {
+		router.push('create-status-sequence');
+	};
+
 	return (
 		<Card p={4}>
 			<HStack paddingBottom={2}>
 				<Heading as='h4' size='md' marginBottom={4}>Цепочки статусов</Heading>
-				<Button colorScheme="green" onClick={onOpen} marginLeft={"auto"}>
+				<Button colorScheme="green" onClick={onCreate} marginLeft={"auto"}>
 					<Add/>
 				</Button>
 			</HStack>
 			<Flex justifyContent={"left"} flexDirection={"column"}>
 				{
-					list.map((sequence, index) => <StatusSequencePreview name={sequence.name} statuses={sequence.statuses} key={index}/>)
+					list.map((sequence, index) => <StatusSequencePreview name={sequence.name} statuses={sequence.statuses} key={index} />)
 				}
 			</Flex>
 		</Card>
